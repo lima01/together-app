92d2388e933c13a73e4b7531015a36ca74848516
Li Ma <lima01@gmail.com>
2023-03-07 22:34:25 -0500

    Change emulator flag to firebaseConfig.useEmulator

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 import * as firebaseConfig from '../../firebase.config.json'
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
-if (firebaseConfig.location.hostname === "localhost") {
+if (firebaseConfig.useEmulator) {
   db.useEmulator("localhost", 8080);
 }
 export default firebase;
